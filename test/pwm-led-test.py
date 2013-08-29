9d0678ef815eed96b84a4cbb07c8f06469d1d027
sfusco <sfusco@ricin.local>
2013-08-29 01:42:35 -0400

    code cleanup

--- test/pwm-led-test.py
+++ test/pwm-led-test.py
@@ -12,12 +12,12 @@ BLUE_PIN  = 15  # GPIO 22
 LED_PINS = [RED_PIN, GREEN_PIN, BLUE_PIN]
 
 PWM_FREQUENCY = 60 # update frequency in Hz
- 
+
 def led_setup():
   # Set up the wiring
   GPIO.setmode(GPIO.BOARD)
 
-  # Setup pins 
+  # Setup pins
   GPIO.setup(RED_PIN, GPIO.OUT)
   GPIO.setup(GREEN_PIN, GPIO.OUT)
   GPIO.setup(BLUE_PIN, GPIO.OUT)
@@ -26,14 +26,14 @@ def led_setup():
   RED_PWM = GPIO.PWM(RED_PIN, PWM_FREQUENCY)
   GREEN_PWM = GPIO.PWM(GREEN_PIN, PWM_FREQUENCY)
   BLUE_PWM = GPIO.PWM(BLUE_PIN, PWM_FREQUENCY)
-  
+
   # start pulse width at a duty cycle of 0 (off)
   RED_PWM.start(0)
   GREEN_PWM.start(0)
   BLUE_PWM.start(0)
 
   return [RED_PWM, GREEN_PWM, BLUE_PWM]
- 
+
 def led_cleanup(rgb_pwm):
   for pwm in rgb_pwm:
     pwm.stop()
@@ -42,7 +42,7 @@ def led_show_rgb(rgb_pwm, rgb_color):
   r = (rgb_color >> 16) & 0xFF
   g = (rgb_color >> 8) & 0xFF
   b = rgb_color & 0xFF
-  
+
   rgb_pwm[0].ChangeDutyCycle(100 * r / 255)
   rgb_pwm[1].ChangeDutyCycle(100 * g / 255)
   rgb_pwm[2].ChangeDutyCycle(100 * b / 255)
@@ -50,7 +50,7 @@ def led_show_rgb(rgb_pwm, rgb_color):
 def led_change_brightness(rgb_pwm, freq):
   for pwm in rgb_pwm:
     pwm.ChangeFrequency(freq)
- 
+
 def main():
   rgb_pwm = led_setup()
 
@@ -68,10 +68,10 @@ def main():
     led_show_rgb(rgb_pwm, rgb_color)
     time.sleep(5)
 
-  while 1:
-    rgb_color = int(raw_input("enter a hex value: "), 0)
-    led_show_rgb(rgb_pwm, rgb_color)
-
+  try:
+    while 1:
+      rgb_color = int(raw_input("enter a hex value: "), 0)
+      led_show_rgb(rgb_pwm, rgb_color)
 
     # for freq in range(1,60):
     #   led_change_brightness(rgb_pwm, freq)
@@ -80,12 +80,12 @@ def main():
     #   led_change_brightness(rgb_pwm, freq)
     #   time.sleep(.3)
 
-  led_cleanup(rgb_pwm)
+  except KeyboardInterrupt:
+    led_cleanup(rgb_pwm)
+    GPIO.cleanup()
 
-try:  
+try:
   main()
-except KeyboardInterrupt:
-  GPIO.cleanup()
 except Exception as ex:
   print ex
   GPIO.cleanup()
